8051677d31d0065075854c389e57e71c70787356
IsmaelSouCar <ismsoucar@alum.us.es>
2026-02-21 18:42:16 +0100

    Casa 21/02 - Library A

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,16 +1,22 @@
-interface Book {
+interface IProps {
+    ISBN : string;
     title: string;
     author: string;
-    date: string;
+    published: string;
     cover: string;
+    status: string;
 }
 
-function Book() {
+function Book(props: IProps) {
   return (
-    <div className="bg-white shadow-md rounded-lg m-4 w-64 h-80 flex flex-col items-center">
+    <div className="bg-white shadow-md rounded-lg m-4 w-64 h-80 flex flex-col items-center justify-between p-2">
         <div className="h-50 w-full rounded-lg overflow-hidden flex items-center justify-center">
-            <img src="src/assets/61xfFSgNfBL._AC_UF1000,1000_QL80_.jpg" alt="Book Cover" className="w-full h-full object-cover" />
+            <img src={props.cover} alt="Book Cover" className="w-full h-full object-cover" />
         </div>
+            <p className="font-bold text-center">{props.title}</p>
+            <p className="text-sm text-gray-500">{props.author}</p>
+            <p className="text-xs text-gray-400">Publicado: {props.published}</p>
+            <span className={`text-xs font-semibold ${props.status === 'Leído' ? 'text-green-500 bg-green-200' : 'text-orange-500 bg-orange-200'} px-1 py-0.5 rounded`}>{props.status}</span>
     </div>
   );
 }
--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -3,8 +3,15 @@ import Book from './Book';
 
 function Library() {
   return (
-    <div>
-      <Book />
+    <div className="grid grid-cols-4 gap-5 p-5">
+      <Book cover="src/assets/cuarto_mono_cover.jpg" title="El Cuarto Mono" author="J. D. Barker" published="27/06/2017" status="Pendiente" ISBN={''}/>
+      <Book cover="src/assets/cuarto_mono_cover.jpg" title="El Cuarto Mono" author="J. D. Barker" published="27/06/2017" status="Leído" ISBN={''}/>
+      <Book cover="src/assets/cuarto_mono_cover.jpg" title="El Cuarto Mono" author="J. D. Barker" published="27/06/2017" status="Pendiente" ISBN={''}/>
+      <Book cover="src/assets/cuarto_mono_cover.jpg" title="El Cuarto Mono" author="J. D. Barker" published="27/06/2017" status="Pendiente" ISBN={''}/>
+      <Book cover="src/assets/cuarto_mono_cover.jpg" title="El Cuarto Mono" author="J. D. Barker" published="27/06/2017" status="Pendiente" ISBN={''}/>
+      <Book cover="src/assets/cuarto_mono_cover.jpg" title="El Cuarto Mono" author="J. D. Barker" published="27/06/2017" status="Pendiente" ISBN={''}/>
+      <Book cover="src/assets/cuarto_mono_cover.jpg" title="El Cuarto Mono" author="J. D. Barker" published="27/06/2017" status="Pendiente" ISBN={''}/>
+      <Book cover="src/assets/cuarto_mono_cover.jpg" title="El Cuarto Mono" author="J. D. Barker" published="27/06/2017" status="Pendiente" ISBN={''}/>
     </div>
   );
 }
