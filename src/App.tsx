6dbe170bec9a860d57f4fba7b38357ee5138314c
IsmaelSoutto <isoucar104@g.educaand.es>
2026-03-11 20:10:10 +0100

    Clase 11/03 - PG-H1P7 N/A

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import './App.css'
 import './components/Header.tsx'
 import Header from './components/Header'
 import Library from './pages/Library'
+import { IBook } from './types/Interfaces'
 
 function App() {
+
+  const books: IBook[] = [
+    {
+      ISBN: '',
+      title: 'El Cuarto Mono',
+      author: 'J. D. Barker',
+      published: '27/06/2017',
+      cover: 'src/assets/cuarto_mono_cover.jpg',
+      status: 'Pendiente'
+    }
+  ];
 
   return (
     <>
     <div className = "min-h-screen">
       <Header />
-      <Library />
+      <Library books={books} />
     </div>
     </>
   )
---
+++ src/types/Interfaces.ts
@@ -0,0 +1,8 @@
+export interface IBook {
+    ISBN : string;
+    title: string;
+    author: string;
+    published: string;
+    cover: string;
+    status: string;
+}
